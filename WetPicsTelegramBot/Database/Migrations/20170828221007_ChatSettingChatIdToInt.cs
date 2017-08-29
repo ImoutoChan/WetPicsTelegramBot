f263b6a81ef4ec066bc5b4138e536e86a0d2dad3
ImoutoChan <kirirrin@gmail.com>
2017-08-29 21:01:45 +0300

    Rewrite migration

--- WetPicsTelegramBot/Database/Migrations/20170828221007_ChatSettingChatIdToInt.cs
+++ WetPicsTelegramBot/Database/Migrations/20170828221007_ChatSettingChatIdToInt.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 using System.Collections.Generic;
+using WetPicsTelegramBot.Helpers;
 
 namespace WetPicsTelegramBot.Database.Migrations
 {
@@ -8,22 +9,12 @@ namespace WetPicsTelegramBot.Database.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<long>(
-                name: "ChatId",
-                table: "ChatSettings",
-                type: "int8",
-                nullable: false,
-                oldClrType: typeof(string));
+            migrationBuilder.ChangeColumnType<long>("ChatSettings", "ChatId", "bigint");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "ChatId",
-                table: "ChatSettings",
-                nullable: false,
-                oldClrType: typeof(long),
-                oldType: "int8");
+            migrationBuilder.ChangeColumnType<string>("ChatSettings", "ChatId", "text");
         }
     }
 }
