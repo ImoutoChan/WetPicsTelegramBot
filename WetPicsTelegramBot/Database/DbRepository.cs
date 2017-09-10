2a18dfb5a26d6c575410e6555f6076b75fa21fba
ImoutoChan <kirirrin@gmail.com>
2017-09-11 01:09:53 +0300

    Remove unused usings

--- WetPicsTelegramBot/Database/DbRepository.cs
+++ WetPicsTelegramBot/Database/DbRepository.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Remotion.Linq.Clauses;
 using WetPicsTelegramBot.Database.Context;
 using WetPicsTelegramBot.Database.Model;
 using WetPicsTelegramBot.Models;
--- WetPicsTelegramBot/Database/Migrations/20170829182822_ChangeStringToNumeric.cs
+++ WetPicsTelegramBot/Database/Migrations/20170829182822_ChangeStringToNumeric.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
 using WetPicsTelegramBot.Helpers;
 
 namespace WetPicsTelegramBot.Database.Migrations
