18834d338f5631569ea761f95851df4774e094e5
ImoutoChan <kirirrin@gmail.com>
2019-01-27 01:19:22 +0300

    Add test project

---
+++ WetPicsTelegramBot.Tests/ImageResizingTests.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using Microsoft.Extensions.Logging.Abstractions;
+using WetPicsTelegramBot.WebApp.Services;
+using Xunit;
+
+namespace WetPicsTelegramBot.Tests
+{
+    public class ImageResizingTests
+    {
+        [Fact]
+        public void ResizeSampleImage()
+        {
+            var inputImage = @"Q:\!playground\wetpicsbot-image-resizing-test\original.jpg";
+            var outputImage = @"Q:\!playground\wetpicsbot-image-resizing-test\resized.jpg";
+
+            var preparer = new TelegramImagePreparing(NullLogger<TelegramImagePreparing>.Instance);
+
+            var stream = File.OpenRead(inputImage);
+            var preparedImage = preparer.Prepare(stream, stream.Length);
+
+            var outStream = File.OpenWrite(outputImage);
+
+            preparedImage.CopyTo(outStream);
+            preparedImage.Dispose();
+            stream.Dispose();
+            outStream.Dispose();
+        }
+    }
+}
