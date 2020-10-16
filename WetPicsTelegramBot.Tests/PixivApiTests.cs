a8ab842c46a6f2a2304c1fd046f74229427cf48e
ImoutoChan <kirirrin@gmail.com>
2020-10-16 19:44:03 +0300

    Change user agent for pixiv

---
+++ WetPicsTelegramBot.Tests/PixivApiTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using PixivApi;
+using Xunit;
+
+namespace WetPicsTelegramBot.Tests
+{
+    public class PixivApiTests
+    {
+        [Fact]
+        public async Task TestAuth()
+        {
+            //var auth = await Auth.AuthorizeAsync(
+            //    "flandere",
+            //    "p33824460",
+            //    "***REMOVED***",
+            //    "***REMOVED***");
+
+            //var latest = await auth.GetLatestWorksAsync();
+
+            //Assert.NotNull(latest);
+        }
+    }
+}
