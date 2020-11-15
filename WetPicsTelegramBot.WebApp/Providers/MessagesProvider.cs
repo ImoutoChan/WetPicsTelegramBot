e07054d516ff7d17615f1376677aa109ab16e0ce
ImoutoChan <kirirrin@gmail.com>
2020-11-15 11:02:10 +0300

    Bump version

--- WetPicsTelegramBot.WebApp/Providers/MessagesProvider.cs
+++ WetPicsTelegramBot.WebApp/Providers/MessagesProvider.cs
@@ -102,7 +102,10 @@ namespace WetPicsTelegramBot.WebApp.Providers
         public string IqdbNotFound => "К сожалению, похожие изображения не найдены.";
 
         public ReplyMessage ChangeLogMessage
-            => new ReplyMessage($"<b>2.9.0</b>{_nl}" +
+            => new ReplyMessage($"<b>2.10.0</b>{_nl}" +
+                                $"* Обновлен netcore до версии 5.0.{_nl}{_nl}" +
+                                
+                                $"<b>2.9.0</b>{_nl}" +
                                 $"* Изменен алгоритм работы с пиксивом, возможно он починится.{_nl}{_nl}" +
                                 
                                 $"<b>2.8.4</b>{_nl}" +
